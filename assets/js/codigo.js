dc06f07b5b526b35ff82e5e3ca29795de3e6dc05
Marya Eduarda <maryacomy.13@gmail.com>
2023-09-25 19:14:01 -0300

    finalmente acabou

--- assets/js/codigo.js
+++ assets/js/codigo.js
@@ -1,1 +1,36 @@
 console.log('Teste do Js')
+
+function getusuarios(){
+
+fetch("https://jsonplaceholder.typicode.com/users").then( (resp1)=> resp1.json() ).then( (resp2) => mostrausuarios(resp2) )
+
+}
+
+function mostrausuarios(dados){
+console.log(dados);
+var container = document.getElementById('usuarios');
+dados.forEach( (usuario) => {
+    let paragrafo = document.createElement('p');
+    paragrafo.innerText = usuario.name;
+    container.appendChild(paragrafo);
+});
+
+
+}
+
+
+
+
+function getfoto(){
+    fetch("https://picsum.photos/200/300").then((resp)=>mostrarfoto(resp))
+}
+function mostrarfoto(dado) {
+    console.log(dado.url);
+let container = document.getElementById('fotuu');
+let imagem = document.createElement('img');
+imagem.src = dado.url;
+container.appendChild(imagem);
+}
+
+getusuarios();
+getfoto();
